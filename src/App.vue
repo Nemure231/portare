36137863c3587dec0acda83ad5f317e889154096
Nemure231 <neruhi5321@gmail.com>
2022-04-24 06:48:30 +0700

    fix main

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,13 @@
 import { StarportCarrier, Starport } from 'vue-starport'
 import { UseDark } from '@vueuse/components'
 import Profile from './components/Data/Profile.vue'
+import Home from './components/Base/Index.vue'
 import Skill from './components/Data/Skill.vue'
 import Portfolio from './components/Data/Portfolio.vue'
 import Work from './components/Data/Work.vue'
 import ReOff from './components/Reload/Index.vue'
 export default {
-  components: { StarportCarrier, Starport, UseDark, Profile, Skill, Portfolio, Work, ReOff },
+  components: { StarportCarrier, Starport, UseDark, Profile, Skill, Portfolio, Work, ReOff, Home },
   data() {
     return {
       switchTheme: {
@@ -81,7 +82,9 @@ export default {
   <router-view @childReload="reloadOnce" name="Work"></router-view>
   <router-view @childReload="reloadOnce" name="Skill"></router-view>
   <router-view @childReload="reloadOnce" name="Portfolio"></router-view>
-  <router-view name="Home"></router-view>
+  <Home v-if="this.$route.name == 'Home'"/>
+  <!-- <router-view name="Home"></router-view> -->
+  
 
   <StarportCarrier></StarportCarrier>
 
